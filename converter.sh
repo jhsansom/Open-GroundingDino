860ca4b856f0dcedca7f37b3ea06fba68527aae6
jhsansom <jhsansom@utexas.edu>
2024-04-27 19:42:42 -0400

    Mixture training configuration

---
+++ converter.sh
@@ -0,0 +1,3 @@
+python tools/coco2odvg.py \
+    --input=/scratch/eecs545w24_class_root/eecs545w24_class/shared_data/dinosaur/synthetic_data/split_datasets/RefCOCO_3ds_7k/test/instances.json \
+    --output=/scratch/eecs545w24_class_root/eecs545w24_class/shared_data/dinosaur/synthetic_data/split_datasets/RefCOCO_3ds_7k/test/instances_odvg.json
