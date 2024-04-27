bf7d388d75a27bcdbff68cc54dc1880812d29e5e
jhsansom <jhsansom@utexas.edu>
2024-04-27 19:21:09 -0400

    Added train/val/test splitting; added random subset

--- split_dataset.py
+++ split_dataset.py
@@ -1,6 +1,8 @@
 import pickle
 import json
 import tqdm
+import numpy as np
+import os
 
 spatial_terms = [
     'right',
@@ -54,14 +56,51 @@ class DatasetSplitter:
         instances_path = self.out_folder + 'instances.json'
         refs_path = self.out_folder + 'refs.json'
 
+        if not os.path.exists(self.out_folder):
+            os.makedirs(self.out_folder)
+
         with open(instances_path, 'w') as fp:
             json.dump(self.instances_data, fp)
         with open(refs_path, 'w') as fp:
             json.dump(self.refs_data, fp)
 
+    # Get train, val, test split 
+    def get_tvt_split(self, split):
+        refs = []
+        for ref in self.refs_data:
+            if ref['split'] == split:
+                refs.append(ref)
+        self.refs_data = refs
+
     # Load a randomized subset of the data
-    def get_random_subset(self):
-        pass
+    def get_random_subset(self, num_images):
+        num_current_images = len(self.instances_data['images'])
+        assert num_images < num_current_images
+
+        random_subset = np.random.randint(0, high=num_current_images, size=num_images)
+
+        self.instances_data['images'] = [self.instances_data['images'][idx] for idx in random_subset]
+
+        valid_image_ids = [img['id'] for img in self.instances_data['images']]
+
+        anns = []
+        print('Removing annotations that dont correspond to valid image')
+        for ann in tqdm.tqdm(self.instances_data['annotations']):
+            if ann['image_id'] in valid_image_ids:
+                anns.append(ann)
+
+        self.instances_data['annotations'] = anns
+
+        valid_ann_ids = [a['id'] for a in self.instances_data['annotations']]
+
+        refs = []
+        print('Removing references that dont correspond to valid annotation')
+        for ref in tqdm.tqdm(self.refs_data):
+            if ref['ann_id'] in valid_ann_ids:
+                refs.append(ref)
+
+        self.refs_data = refs
+
 
     # Restrict annotations to those with corresponding references
     def get_ann_w_refs(self):
@@ -100,12 +139,17 @@ class DatasetSplitter:
 if __name__ == '__main__':
     #INSTANCES_PATH = "/scratch/eecs545w24_class_root/eecs545w24_class/shared_data/dinosaur/refcoco/refcoco/instances.json"
     #REFS_PATH = "/scratch/eecs545w24_class_root/eecs545w24_class/shared_data/dinosaur/refcoco/refcoco/refs(google).p"
-    INSTANCES_PATH = "/scratch/eecs545w24_class_root/eecs545w24_class/shared_data/dinosaur/refcoco_split/valid_annotations/instances.json"
-    REFS_PATH = "/scratch/eecs545w24_class_root/eecs545w24_class/shared_data/dinosaur/refcoco_split/valid_annotations/refs.json"
-    OUT_FOLDER = "/scratch/eecs545w24_class_root/eecs545w24_class/shared_data/dinosaur/refcoco_split/non_spatial/"
+    #FOLDER_PATH = "/scratch/eecs545w24_class_root/eecs545w24_class/shared_data/dinosaur/synthetic_data/split_datasets/RefCOCO_3ds_7k/train/"
+    FOLDER_PATH = "/scratch/eecs545w24_class_root/eecs545w24_class/shared_data/dinosaur/refcoco_split/valid_annotations/val/"
+    INSTANCES_PATH = FOLDER_PATH + "instances.json"
+    REFS_PATH = FOLDER_PATH + "refs.json"
+    OUT_FOLDER = "/scratch/eecs545w24_class_root/eecs545w24_class/shared_data/dinosaur/refcoco_split/nonspatial" + '/' # <--- include this slash
 
     ds = DatasetSplitter(INSTANCES_PATH, REFS_PATH, OUT_FOLDER)
     #ds.get_ann_w_refs()
     ds.split_spatial_nonspatial(spatial=False)
+    #ds.print_dataset_statistics()
+    #ds.get_tvt_split('train')
+    #ds.get_random_subset(5000)
     ds.print_dataset_statistics()
     ds.save_data()
