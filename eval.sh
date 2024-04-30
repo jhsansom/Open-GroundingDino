475b41ffcad3156f61bf7ed621b4e152aff55c0a
Aditya Vasudevan <adivasu@umich.edu>
2024-04-29 23:03:44 -0400

    Eval script for Billy

--- eval.sh
+++ eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=test_job_eecs545
-#SBATCH --mail-user=jhsansom@umich.edu
+#SBATCH --job-name=ewc_eval
+#SBATCH --mail-user=adivasu@umich.edu
 #SBATCH --mail-type=END
 #SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
@@ -9,15 +9,16 @@
 #SBATCH --mem-per-cpu=30g
 #SBATCH --gres=gpu:1
 #SBATCH --time=06:00:00
-#SBATCH --account=eecs545w24_class
+#SBATCH --account=eecs602w24_class
 #SBATCH --partition=spgpu
 #SBATCH --output=./jobs/%u/%x-%j.log
 
-module load cuda
-source ../cleanrl-env/bin/activate
+# module load cuda
+# source ../cleanrl-env/bin/activate
 
 cd GroundingDINO
-pip install -q -e .
+python setup.py develop
+# pip install -e .
 cd ..
 
 # Epoch -1 (prior to any training)
@@ -26,44 +27,12 @@ python eval.py --real --spatial --weights=$WEIGHTS # refcoco spatial
 python eval.py --real --weights=$WEIGHTS # refcoco nonspatial
 python eval.py --weights=$WEIGHTS # synthetic validation
 
-# Epoch 0 (after 1 epoch of training)
-WEIGHTS="./logs_mixture/checkpoint0000.pth"
-python eval.py --real --spatial --weights=$WEIGHTS # refcoco spatial
-python eval.py --real --weights=$WEIGHTS # refcoco nonspatial
-python eval.py --weights=$WEIGHTS # synthetic validation
+LOCATION="/scratch/eecs545w24_class_root/eecs545w24_class/shared_data/dinosaur/model_checkpoints/ewc_mixed_checkpoints"
 
-# Epoch 1 (after 2 epochs of training)
-WEIGHTS="./logs_mixture/checkpoint0001.pth"
-python eval.py --real --spatial --weights=$WEIGHTS # refcoco spatial
-python eval.py --real --weights=$WEIGHTS # refcoco nonspatial
-python eval.py --weights=$WEIGHTS # synthetic validation
-
-# Epoch 2 (after 3 epochs of training)
-WEIGHTS="./logs_mixture/checkpoint0002.pth"
-python eval.py --real --spatial --weights=$WEIGHTS # refcoco spatial
-python eval.py --real --weights=$WEIGHTS # refcoco nonspatial
-python eval.py --weights=$WEIGHTS # synthetic validation
-
-# Epoch 3 (after 4 epochs of training)
-WEIGHTS="./logs_mixture/checkpoint0003.pth"
-python eval.py --real --spatial --weights=$WEIGHTS # refcoco spatial
-python eval.py --real --weights=$WEIGHTS # refcoco nonspatial
-python eval.py --weights=$WEIGHTS # synthetic validation
-
-# Epoch 4 (after 5 epochs of training)
-WEIGHTS="./logs_mixture/checkpoint0004.pth"
-python eval.py --real --spatial --weights=$WEIGHTS # refcoco spatial
-python eval.py --real --weights=$WEIGHTS # refcoco nonspatial
-python eval.py --weights=$WEIGHTS # synthetic validation
-
-# Epoch 5 (after 6 epochs of training)
-WEIGHTS="./logs_mixture/checkpoint0005.pth"
-python eval.py --real --spatial --weights=$WEIGHTS # refcoco spatial
-python eval.py --real --weights=$WEIGHTS # refcoco nonspatial
-python eval.py --weights=$WEIGHTS # synthetic validation
-
-# Epoch 6 (after 7 epochs of training)
-WEIGHTS="./logs_mixture/checkpoint0006.pth"
-python eval.py --real --spatial --weights=$WEIGHTS # refcoco spatial
-python eval.py --real --weights=$WEIGHTS # refcoco nonspatial
-python eval.py --weights=$WEIGHTS # synthetic validation
+for i in 0 1 2 3 4 5 6;
+do
+    WEIGHTS="$LOCATION/checkpoint000$i.pth"
+    python eval.py --real --spatial --weights=$WEIGHTS # refcoco spatial
+    python eval.py --real --weights=$WEIGHTS # refcoco nonspatial
+    python eval.py --weights=$WEIGHTS # synthetic validation
+done
